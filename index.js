bda0963614de07c6032fce8178affa4a8d9a1c96
Chris <chris_alderson@hmamail.com>
2016-04-12 01:07:31 +0200

    done?

--- index.js
+++ index.js
@@ -8,132 +8,126 @@ var querystring = require('querystring');
 var request = require('request');
 var sanitize = require('butter-sanitize');
 
-var MovieApi = function(args) {
-  var that = this;
+var MovieApi = function (args) {
+    var that = this;
 
-  MovieApi.super_.call(this);
+    MovieApi.super_.call(this);
 
-  if (args.apiURL) this.apiURL = args.apiURL.split(',');
+    if (args.apiURL) this.apiURL = args.apiURL.split(',');
 
-  this.language = args.language;
-  this.quality = args.quality;
-  this.translate = args.translate;
+    this.language = args.language;
+    this.quality = args.quality;
+    this.translate = args.translate;
 };
 
 inherits(MovieApi, Generic);
 
 MovieApi.prototype.config = {
-  name: 'MovieApi',
-  uniqueId: 'imdb_id',
-  tabName: 'MovieApi',
-  type: 'movie',
-  metadata: 'trakttv:movie-metadata'
+    name: 'MovieApi',
+    uniqueId: 'imdb_id',
+    tabName: 'MovieApi',
+    type: 'movie',
+    metadata: 'trakttv:movie-metadata'
 };
 
-MovieApi.prototype.extractIds = function(items) {
-  return _.pluck(items.results, 'imdb_id');
+MovieApi.prototype.extractIds = function (items) {
+    return _.pluck(items.results, 'imdb_id');
 };
 
-MovieApi.prototype.fetch = function(filters) {
-  var that = this;
+function format(movies) {
+    var results = _.chain(movies).map(function (movie) {
+        return {
+            type: 'movie',
+            imdb_id: movie.imdb_id,
+            title: movie.title,
+            year: movie.year,
+            genre: movie.genres,
+            rating: parseInt(movie.rating, 10) / 10,
+            runtime: movie.runtime,
+            image: movie.image,
+            cover: movie.cover,
+            backdrop: movie.backdrop,
+            synopsis: movie.synopsis,
+            trailer: movie.trailer || false,
+            certification: movie.certification,
+            torrents: movie.torrents
+        };
+    }).value();
 
-  var params = {};
-  params.sort = 'seeds';
-  params.limit = '50';
+    return {
+        results: sanitize(results),
+        hasMore: true
+    };
+};
 
-  filters.page = 1;
-
-  if (filters.keywords) {
-    params.keywords = filters.keywords.replace(/\s/g, '% ');
-  }
-
-  if (filters.genre) {
-    params.genre = filters.genre;
-  }
-
-  if (filters.order) {
-    params.order = filters.order;
-  }
-
-  if (filters.sorter && filters.sorter !== 'popularity') {
-    params.sort = filters.sorter;
-  }
-
-  function getFetch(index, url, that) {
+function get(index, url, that) {
     var deferred = Q.defer();
 
     var options = {
-      url: url,
-      json: true
+        url: url,
+        json: true
     };
 
     var req = _.extend({}, that.apiURL[index], options);
     console.info('Request to MovieApi', req.url);
-    request(req, function(err, res, data) {
-      if (err || res.statusCode >= 400) {
-        console.warn('MovieAPI endpoint \'%s\' failed.', that.apiURL[index]);
-        if (index + 1 >= that.apiURL.length) {
-          return deferred.reject(err || 'Status Code is above 400');
+    request(req, function (err, res, data) {
+        if (err || res.statusCode >= 400) {
+            console.warn('MovieAPI endpoint \'%s\' failed.', that.apiURL[index]);
+            if (index + 1 >= that.apiURL.length) {
+                return deferred.reject(err || 'Status Code is above 400');
+            } else {
+                return getFetch(index + 1, url);
+            }
+        } else if (!data || data.error) {
+            err = data ? data.status_message : 'No data returned';
+            console.error('API error:', err);
+            return deferred.reject(err);
         } else {
-          return get(index + 1, url);
+            return deferred.resolve(format(data));
         }
-      } else if (!data || data.error) {
-        err = data ? data.status_message : 'No data returned';
-        console.error('API error:', err);
-        return deferred.reject(err);
-      } else {
-        return deferred.resolve({
-          results: sanitize(data),
-          hasMore: true
-        });
-      }
     });
 
     return deferred.promise;
-  };
-
-  var index = 0;
-  var url = that.apiURL[index] + 'movies/' + filters.page + '?' + querystring.stringify(params).replace(/%25%20/g, '%20');
-  return getFetch(index, url, that);
 };
 
-MovieApi.prototype.detail = function(torrent_id, old_data, debug) {
-  var that = this;
-  debug === undefined ? debug = true : '';
+MovieApi.prototype.fetch = function (filters) {
+    var that = this;
 
-  function getDetail(index, url, that) {
-    var deferred = Q.defer();
+    var params = {};
+    params.sort = 'seeds';
+    params.limit = '50';
 
-    var options = {
-      url: url,
-      json: true
-    };
+    if (filters.keywords) {
+        params.keywords = filters.keywords.replace(/\s/g, '% ');
+    }
 
-    var req = _.extend({}, that.apiURL[index], options);
-    console.info('Request to MovieApi', req.url);
-    request(req, function(err, res, data) {
-      if (err || res.statusCode >= 400) {
-        console.warn('MovieAPI endpoint \'%s\' failed.', that.apiURL[index]);
-        if (index + 1 >= that.apiURL.length) {
-          return deferred.reject(err || 'Status Code is above 400');
-        } else {
-          return get(index + 1, url);
-        }
-      } else if (!data || data.error) {
-        err = data ? data.status_message : 'No data returned';
-        console.error('API error:', err);
-        return deferred.reject(err);
-      } else {
-        return deferred.resolve(sanitize(data));
-      }
-    });
+    if (filters.genre) {
+        params.genre = filters.genre;
+    }
 
-    return deferred.promise;
-  };
+    if (filters.order) {
+        params.order = filters.order;
+    }
 
-  var index = 0;
-  var url = that.apiURL[index] + 'movie/' + torrent_id;
-  return getDetail(index, url, that);
+    if (filters.sorter && filters.sorter !== 'popularity') {
+        params.sort = filters.sorter;
+    }
+
+    var index = 0;
+    var url = that.apiURL[index] + 'movies/' + filters.page + '?' + querystring.stringify(params).replace(/%25%20/g, '%20');
+    return get(index, url, that);
+};
+
+
+MovieApi.prototype.random = function () {
+    var that = this;
+    var index = 0;
+    var url = that.apiURL[index] + '/random/movie';
+    return get(index, url, that);
+};
+
+MovieApi.prototype.detail = function (torrent_id, old_data, debug) {
+    return Q(old_data);
 };
 
 module.exports = MovieApi;
